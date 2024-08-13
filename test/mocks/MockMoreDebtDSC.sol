41acf587e17a0ffbd33ace2b430658b2c7e35418
yudengke <yudengke1016@gmail.com>
2024-08-13 16:42:47 +0800

    add fuzz testing

--- test/mocks/MockMoreDebtDSC.sol
+++ test/mocks/MockMoreDebtDSC.sol
@@ -30,13 +30,6 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {MockV3Aggregator} from "./MockV3Aggregator.sol";
 
 /*
- * @title DecentralizedStableCoin
- * @author Patrick Collins
- * Collateral: Exogenous
- * Minting (Stability Mechanism): Decentralized (Algorithmic)
- * Value (Relative Stability): Anchored (Pegged to USD)
- * Collateral Type: Crypto
- *
 * This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the
 DSCEngine smart contract.
  */
