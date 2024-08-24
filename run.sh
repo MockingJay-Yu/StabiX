029594f381428b34e3ef2e2dbd4040a8e6734e88
yudengke <yudengke1016@gmail.com>
2024-08-25 02:45:16 +0800

    update

--- run.sh
+++ run.sh
@@ -2,8 +2,14 @@ source .env
 
 if [ "$1" == "sepolia" ]; then
     RPC_URL=$SEPOLIA_RPC_URL
-elif [ "$1" == "mainnet" ]; then
-    RPC_URL=$MAINNET_RPC_URL
+elif [ "$1" == "arbitrum" ]; then
+    RPC_URL=$ARBITRUM_RPC_URL
+fi
+
+if [ "$2" == "sepolia" ]; then
+    PRIVATE_KEY=$SEPOLIA_PRIVATE_KEY
+elif [ "$2" == "arbitrum" ]; then
+    RPC_URL=$POLYGON_PRIVATE_KEY
 fi
 
 forge script script/DeployDSC.s.sol --rpc-url $RPC_URL \
